d9c238c38a9cc54d88c3b4d7b75a7df410b145f9
Paulo Sérgio Baima <psbds_filho@hotmail.com>
2018-04-02 14:17:32 -0300

    Initializing Repository

---
+++ DirectlineBotDemo/DirectlineBotDemo.Web/wwwroot/js/directLineService.js
@@ -0,0 +1,33 @@
+﻿var directLineService = {
+    AUTH_SECRET: "xxxxxxxxxxxxxxxxx",
+    startConversation: function () {
+        return $.ajax({
+            url: "https://directline.botframework.com/v3/directline/conversations",
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + this.AUTH_SECRET
+            }
+        });
+    },
+    sendMessage: function (conversationId, data) {
+        return $.ajax({
+            url: "https://directline.botframework.com/v3/directline/conversations/" + conversationId + "/activities",
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + this.AUTH_SECRET,
+                "Content-Type":"application/json"
+            },
+            data: JSON.stringify(data)
+        });
+    },
+    reconnect: function (conversationId) {
+        return $.ajax({
+            url: "https://directline.botframework.com/v3/directline/conversations/" + conversationId+"?watermark=0",
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + this.AUTH_SECRET,
+                "Content-Type": "application/json"
+            }
+        });
+    }
+}
---
+++ DirectlineBotDemo/DirectlineBotDemo.Web/wwwroot/js/sessionService.js
@@ -0,0 +1,10 @@
+﻿var sessionService = {
+    SESSION_KEY: "DirectLineSession",
+    getSession: function () {
+        var data = sessionStorage.getItem(this.SESSION_KEY);
+        return data ? JSON.parse(data) : data;
+    },
+    setSession: function (data) {
+        return sessionStorage.setItem(this.SESSION_KEY, JSON.stringify(data));
+    }
+}
